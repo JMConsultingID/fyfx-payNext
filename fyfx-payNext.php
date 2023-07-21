b0d494670fbd55ef7e506ba70604b5fbcbc46232
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-07-21 16:48:49 +0800

    Update fyfx-payNext.php

--- fyfx-payNext.php
+++ fyfx-payNext.php
@@ -932,6 +932,47 @@ function check_paynext_transaction_status(){
 
 }
 
+// Additional Script Response
+function paynext_js_script_response() {
+    // Display API response header in inspect element
+    $current_url = $_SERVER['REQUEST_URI'];
+    if (strpos($current_url, '/checkout/order-received/') !== false){
+        $order_id = absint(get_query_var('order-received'));
+        $api_paynext = get_post_meta($order_id, 'error', true);
+        ?>
+        <script>
+            var paynextResponse = <?php echo json_encode($api_paynext); ?>;
+            console.log(paynextResponse);
+        </script>
+        <?php
+    }
+    elseif (strpos($current_url, '/sellkit_step/') !== false && strpos($current_url, '?order-key=wc_order') !== false) {
+        $key = isset( $_GET['order-key'] ) ? sanitize_text_field( $_GET['order-key'] ) : false;
+        $current_page_id = get_queried_object_id();
+        if ( empty( $key ) ) {
+            return;
+        }
+        if ( $key ) {
+            $order_id = wc_get_order_id_by_order_key( $key );
+        }
+        $api_paynext = get_post_meta($order_id, 'error', true);
+        ?>
+        <script>
+            var paynextResponse = <?php echo json_encode($api_paynext); ?>;
+            console.log(paynextResponse);
+        </script>
+        <?php
+    }
+    else{
+        return;
+    }
+}
+do_action('paynext_hook_paynext_js_script_response');
+add_action('paynext_hook_paynext_js_script_response', 'paynext_js_script_response');
 
+function run_paynext_js_script_response() {
+    do_action('paynext_hook_paynext_js_script_response');
+}
+add_action('wp_footer', 'run_paynext_js_script_response');
 
 ?>
