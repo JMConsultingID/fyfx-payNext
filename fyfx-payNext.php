071e42d18766d8dbe011c3a6f2da4451c546978b
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-09 14:18:59 +0800

    Update fyfx-payNext.php

--- fyfx-payNext.php
+++ fyfx-payNext.php
@@ -592,8 +592,7 @@ function woocommerce_paynext_init()
 
                 } elseif ($status_nm == 2 || $status_nm == 22 || $status_nm == 23) { // 2:Declined/Failed, 22:Expired, 23:Cancelled                  
                     // Add a notice and link to go back to the previous checkout page
-                    wc_add_notice( sprintf( __('Payment Failed - Reason :  Card %s, Please Use 
-            Alternative Debit & Credit Card with Stripe Payment', 'fyfx-payNext'), $status_cc ), 'error' );
+                    wc_add_notice( sprintf( __('We’re sorry, but your payment attempt was unsuccessful. Please consider using an alternative payment method to complete your purchase.', 'fyfx-payNext'), $status_cc ), 'error' );
                     $order->add_order_note('cError: ' . $error . "log: " . $response_encode );
                     $order->update_status($this->status_cancelled);
                     update_post_meta( $order_id, 'transaction_id', $transaction_id );
@@ -602,8 +601,7 @@ function woocommerce_paynext_init()
                     return;
                 } else { // Pending
                     // Add a notice and link to go back to the previous checkout page
-                    wc_add_notice( sprintf( __('Payment Failed - Reason :  Card %s, Please Use 
-            Alternative Debit & Credit Card with Stripe Payment', 'fyfx-payNext'), $status_cc ), 'error' );
+                    wc_add_notice( sprintf( __('We’re sorry, but your payment attempt was unsuccessful. Please consider using an alternative payment method to complete your purchase.', 'fyfx-payNext'), $status_cc ), 'error' );
                     $order->add_order_note('cError: ' . $error . "log: " . $response_encode );
                     $order->update_status($this->status_pending);                  
                     update_post_meta( $order_id, 'payment_status', $status_cc );
