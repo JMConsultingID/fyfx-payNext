3c2dfebd26cb62763a06eb790bdf5695b132fb00
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-09 16:29:57 +0800

    Update custom-script.js

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -63,4 +63,13 @@ jQuery(document).ready(function() {
 });
 
 
-        
+(function($) {
+    // Wait for the document to be ready
+    $(document).ready(function() {
+        // Hook into the WooCommerce notice event
+        $(document.body).on('wc_add_notice', function(event, message, notice_type) {
+            // Scroll to the top of the page
+            $('html, body').animate({ scrollTop: 0 }, 'fast');
+        });
+    });
+})(jQuery);
