4392d8bcb890479a537e550d9ee88c724c45f22f
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-03 14:31:05 +0800

    Update fyfx-payNext.php

--- fyfx-payNext.php
+++ fyfx-payNext.php
@@ -610,7 +610,8 @@ function woocommerce_paynext_init()
                 $results  = json_decode($response, true);
 
                 if (empty($results)){
-                    update_post_meta( $order_id, 'error_payment', 'variable results is empty ' .$results );                    
+                    update_post_meta( $order_id, 'error_payment', 'failed - no response from paynext ' .$results ); 
+                    update_post_meta( $order_id, 'reason', 'Max. transactions allowed within (1 days)' );                   
                     error_log('Payment API response error: Error Response Code : Empty Result - '. $results.'-'. $response);
                     wc_get_logger()->error('WC Payment API response error: Error Response Code : Empty Result - '. $results.'-'. $response);
                     wc_add_notice( sprintf( __('We’re sorry, but your payment attempt was unsuccessful. Please consider using an alternative payment method to complete your purchase. <p>Code : Scrubbed Reason :  Max. transactions allowed within (1 days)</p>', 'fyfx-payNext')), 'error' );
@@ -629,9 +630,9 @@ function woocommerce_paynext_init()
                 $url_auth_url_2 = $results["authurl"];
 
                 if (empty($results["authurl"])){
-                    update_post_meta( $order_id, 'error_payment', 'authurl is empty' );                    
-                    error_log('Payment API response error: ' . print_r($results, true));
-                    wc_get_logger()->error('Payment API response error: ' . print_r($results, true));
+                    update_post_meta( $order_id, 'error_payment', 'failed - authurl is empty -  response from paynext ');                     
+                    error_log('Payment API response error: No Response Auth URL' . print_r($results, true));
+                    wc_get_logger()->error('Payment API response error: No Response Auth URL' . print_r($results, true));
                     wc_add_notice( sprintf( __('We’re sorry, but your payment attempt was unsuccessful. Please consider using an alternative payment method to complete your purchase. <p>Code : Empty Authurl</p>', 'fyfx-payNext')), 'error' );
                     $order->update_status($this->status_pending);
                     return;
