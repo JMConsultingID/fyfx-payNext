8f94dae6ff497033d7ce53c1338f265724cef79e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-03 13:03:45 +0800

    Update fyfx-payNext.php

--- fyfx-payNext.php
+++ fyfx-payNext.php
@@ -601,7 +601,7 @@ function woocommerce_paynext_init()
                 curl_setopt($curl, CURLOPT_POST, 1);
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
                 curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
-                curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+                curl_setopt($curl, CURLOPT_TIMEOUT, 10);
                 curl_setopt($curl, CURLOPT_HEADER, 0);
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
                 curl_setopt($curl, CURLOPT_COOKIE, $curl_cookie);
@@ -654,7 +654,7 @@ function woocommerce_paynext_init()
                     // Arguments for POST request - if you have specific data to post, add it in the 'body' key
                     $args = array(
                         'method' => 'POST',
-                        'timeout' => 45,
+                        'timeout' => 20,
                         'redirection' => 5, // Maximum number of redirects
                         'httpversion' => '1.0',
                         'blocking' => true,
