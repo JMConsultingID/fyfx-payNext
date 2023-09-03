18c50cdb0729823a0034934a07b27dc221d0c704
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-03 13:07:21 +0800

    Update fyfx-payNext.php

--- fyfx-payNext.php
+++ fyfx-payNext.php
@@ -654,7 +654,7 @@ function woocommerce_paynext_init()
                     // Arguments for POST request - if you have specific data to post, add it in the 'body' key
                     $args = array(
                         'method' => 'POST',
-                        'timeout' => 20,
+                        'timeout' => 10,
                         'redirection' => 5, // Maximum number of redirects
                         'httpversion' => '1.0',
                         'blocking' => true,
